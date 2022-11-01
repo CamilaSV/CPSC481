470a00f7e40d38bedf4fe482d7ec6c101f4c8280
sehwa.kim <sehwa.kim@ucalgary.ca>
2022-11-01 10:02:03 -0600

    Change Size

---
+++ CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/UserControl_ChatList.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481Group12FoodyApp
+{
+    /// <summary>
+    /// Interaction logic for UserControl_ChatList.xaml
+    /// </summary>
+    public partial class UserControl_ChatList : UserControl
+    {
+        private PageNavigator navigate_helper;
+
+        public UserControl_ChatList(PageNavigator navigate_helper)
+        {
+            InitializeComponent();
+            this.navigate_helper = navigate_helper;
+        }
+
+        private void List_HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoHomePage();
+        }
+
+        private void List_CalButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoCalendar();
+        }
+
+        private void List_CreateGroup_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoCreateGroup();
+        }
+    }
+}
---
+++ CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/UserControl_Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481Group12FoodyApp
+{
+    /// <summary>
+    /// Interaction logic for UserControl_Login.xaml
+    /// </summary>
+    public partial class UserControl_Login : UserControl
+    {
+        private PageNavigator navigate_helper;
+
+        public UserControl_Login(PageNavigator navigate_helper)
+        {
+            InitializeComponent();
+            this.navigate_helper = navigate_helper;
+        }
+
+        private void Login_LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoChatList();
+        }
+
+        private void Login_BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoStart();
+        }
+    }
+}
