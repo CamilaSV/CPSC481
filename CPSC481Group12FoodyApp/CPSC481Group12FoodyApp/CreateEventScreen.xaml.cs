af89f0375114fa11e41d8df69bb96ded1c4aba68
bonnie-wu <bonnie.wu@ucalgary.ca>
2022-11-11 23:16:59 -0700

    CreateEventScreen created

---
+++ CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/CreateEventScreen.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481Group12FoodyApp
+{
+    /// <summary>
+    /// Interaction logic for Template_WithBottom.xaml
+    /// </summary>
+    public partial class Template_WithBottom : UserControl
+    {
+        private PageNavigator navigate_helper;
+
+        public Template_WithBottom(PageNavigator navigate_helper)
+        {
+            InitializeComponent();
+            this.navigate_helper = navigate_helper;
+        }
+
+        private void Bottom_HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoHomePage();
+        }
+
+        private void Bottom_CalButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoCalendar();
+        }
+
+        private void Bottom_ChatButton_Click(object sender, RoutedEventArgs e)
+        {
+            navigate_helper.gotoChatList();
+        }
+
+        private void Bottom_CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
