ff526e1aa3dc15289ebe6517fc3e209c55ea2cfe
jaydin-1 <47401182+jaydin-1@users.noreply.github.com>
2022-11-21 20:55:20 -0700

    Updated Friend Request Issue still persists - empty friend request DB.

--- CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/AddFriend_FriendRequests/FriendRequestControlDesignModel.cs
+++ CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/AddFriend_FriendRequests/FriendRequestControlDesignModel.cs
@@ -29,51 +29,8 @@ namespace CPSC481Group12FoodyApp
 
         public FriendRequestControlDesignModel()
         {
-            /*
             friendRequestCollection = new ObservableCollection<propertyChange>();
-            propertyChange requestItem = new propertyChange
-            {
-                TargetUserName = "test",
-                Abbreviation = "T",
-            };
-            friendRequestCollection.Add(requestItem);
-            */
-
-            
-            friendRequestCollection = new ObservableCollection<propertyChange>();
-            string currentUserEmail = Logic_Login.getCurrentUserEmail();
-
-            List<string> lines = new List<string>();
-            // ReadAllLines closes file after reading.
-            lines = File.ReadAllLines(PathFinder.getAccFriendReq(currentUserEmail)).ToList();
-
-            if (String.IsNullOrEmpty(currentUserEmail))
-            {
-                propertyChange requestItem = new propertyChange();
-                friendRequestCollection.Add(requestItem);
-            }
-            else if (String.IsNullOrEmpty(lines[0]))
-            {
-                propertyChange requestItem = new propertyChange();
-                friendRequestCollection.Add(requestItem);
-            }
-            else
-            {
-            //foreach (string line in lines)
-            //{
-                //string[] currentUserName = line.Split('@');
-                //string nameAbbreviation = currentUserName[0].Substring(0, 1);
-                propertyChange requestItem = new propertyChange
-                {
-                    //TargetUserName = currentUserName[0],
-                    TargetUserName = "test",
-                    Abbreviation = "T",
-                };
-                friendRequestCollection.Add(requestItem);
-            }
-
-        
-        
+            Logic_Profle.displayUsersFriendRequest(friendRequestCollection);
 
             // for testing purposes.
 
@@ -92,7 +49,6 @@ namespace CPSC481Group12FoodyApp
                 },
             };
             */
-
         }
     }
 }
