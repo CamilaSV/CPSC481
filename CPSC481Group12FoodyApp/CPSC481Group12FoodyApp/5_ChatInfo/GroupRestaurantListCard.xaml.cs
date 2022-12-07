2dfee22a365eb77d6a27f28a4cc7d77867b92713
jaydin-1 <47401182+jaydin-1@users.noreply.github.com>
2022-12-07 00:27:47 -0700

    broken

--- CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/5_ChatInfo/GroupRestaurantListCard.xaml.cs
+++ CPSC481Group12FoodyApp/CPSC481Group12FoodyApp/5_ChatInfo/GroupRestaurantListCard.xaml.cs
@@ -22,17 +22,30 @@ namespace CPSC481Group12FoodyApp
     /// </summary>
     public partial class GroupRestaurantListCard : UserControl
     {
+        
         public GroupRestaurantListCard()
         {
             InitializeComponent();
         }
 
+        /*
+         * Click="vote_Restaurant_Click"
         private void vote_Restaurant_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
+            if (voteButton.IsChecked == true)
+            {
+                Debug.WriteLine("checked");
+                IsUserVote = true;
 
-        
+            }
+            else if (voteButton.IsChecked == false)
+            {
+                Debug.WriteLine("not checked");
+                IsUserVote = false;
+            }
+        }
+        */
+
         private void removeRestaurantButton(object sender, RoutedEventArgs e)
         {
 
@@ -43,14 +56,20 @@ namespace CPSC481Group12FoodyApp
             Debug.WriteLine("Card is clickable");
         }
 
+        
+         
         private void voteButton_Checked(object sender, RoutedEventArgs e)
         {
-
+            Debug.WriteLine("y");
+            //this.voteButton.IsChecked = true;
+            this.voteButton.Background=new SolidColorBrush(Colors.Orange);
         }
 
         private void voteButton_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            Debug.WriteLine("n");
+            this.voteButton.Background = new SolidColorBrush(Colors.RoyalBlue);
+            //this.voteButton.IsChecked = false;
         }
     }
 }
